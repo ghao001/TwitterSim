13ad47faff855671bdfab2d07ca7ea98279a5917
ghao001 <36051327+ghao001@users.noreply.github.com>
2018-07-19 01:08:42 -0700

    Delete GroupInterface.java

--- GroupInterface.java
+++
@@ -1,8 +0,0 @@
-public interface GroupInterface
-{
-    public void setGroup(String in);
-    public String getGroupID();
-    public void removeGroup();
-    public String getID();
-    public int hashCode();
-}
